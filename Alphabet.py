386f9197dfdf161454c8684b6307571ba3246113
Tyler Robbins <tyler@familyrobbins.com>
2014-03-16 14:14:14 -0700

    Begun working on threading for the timer.

--- Alphabet.py
+++ Alphabet.py
@@ -4,9 +4,37 @@
 #Aphabet Game		#
 #####################
 
-import pygame
+import pygame, sys
 from pygame.locals import *
+from string import *
+from multiprocessing.pool import ThreadPool
 
+pygame.init()
 
+def wait(time):
+	pygame.time.wait(time)
+	up = False
+	return up
+
+def timer():
+	thread.start_new_thread(wait, 30000)
+
+print letters
+
+game = True
+numP = int(raw_input("How many people are playing: "))
+clock = pygame.time.Clock()
+pool = ThreadPool(processes = 1)
+up = True
+
+while True:
+	while game:
+		for i in range(numP):
+			raw_input("What is your animal: ")
+			async_result = pool.apply_async(timer, ())
+
+			while return_val
+
+pygame.quit()
 
 nuclear = u'\u2622'
