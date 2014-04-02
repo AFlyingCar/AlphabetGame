2a323004f85e3aa758baa93325c5ff94b09dbc1c
Tyler Robbins <tyler@familyrobbins.com>
2014-04-01 19:10:41 -0700

    Broken. Kept for archival purposes.

--- Alphabet.py
+++ Alphabet.py
@@ -11,7 +11,9 @@ from pygame.locals import *
 from string import *
 import thread
 
-pygame.init()
+GREEN = (16,119,30) #set the color GREEN to a variable
+BLACK = (0,0,0)
+
 class myThread(threading.Thread):
     def __init__(self, threadID, name, q, e):
         threading.Thread.__init__(self)
@@ -30,17 +32,88 @@ def wait(e):
 		pygame.time.delay(10000)
 		e = False
 
-print letters
+def keycheck():
+    if event.type == QUIT:
+        shutdown()
+
+    if event.type == KEYDOWN:
+        print event.unicode
+
+        if event.key == K_ESCAPE:
+            shutdown()
+
+def shutdown():
+    pygame.quit()
+    sys.exit()
+
+pygame.init()
+
 game = True
-numP = 2
+numP = int(raw_input("How many players: "))
 exitFlag = True
+guess = ""
+turn = True
+letter = "a"
+letternum = 0
+keypress = False
+prompt = "Choose an animal that starts with the letter " + letter + ":"
+animals = ["cow", "rabbit"]
 
-while True:
-	while game:
-		for i in range(numP):
-			animal = raw_input("What is your animal: ")
-			thread.start_new_thread(wait, (exitFlag,))
+display = pygame.display.set_mode((640, 480)) #set screen size
+display.fill((GREEN))
+pygame.display.set_caption("Animal Game!") #set window caption
+fontObj = pygame.font.Font('freesansbold.ttf', 29) #Sent game fonts
 
-pygame.quit()
+while True:    
+    while game:
+        promptdisp = fontObj.render(prompt, True, BLACK)
+        
+        for event in pygame.event.get():
+            keycheck()
+
+        pygame.display.update()
+
+        for i in range(numP):
+            while turn:
+                print guess
+                display.fill(GREEN)
+                choice = fontObj.render(">>> " + guess, True, BLACK)
+                display.blit(choice, (0,180))
+                display.blit(promptdisp, (0, 151))
+                
+                for event in pygame.event.get():
+                    keycheck()
+
+                    if event.type == KEYDOWN:
+                        if event.key == K_RETURN:
+                            turn = False
+
+                        if event.key == K_BACKSPACE:
+                            try:
+                                guess = guess[:-1]
+
+                            except Exception:
+                                pass
+
+                        else:
+                            guess += event.unicode
+                
+                pygame.display.update()
+
+            for i in animals:
+                if str(guess).lower() == str(i).lower():
+                    print i
+                    print "Correct!"
+
+                else:
+                    print animals
+                    print i
+                    print guess.lower()
+                    print "meh, wrong!"
+                    shutdown()
+
+            turn = True
+
+shutdown()
 
 nuclear = u'\u2622'
